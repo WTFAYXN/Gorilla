90a8e0caa8b84529aeba4ddf0393c6418ea52de7
wtfayxn <ayansayad2005@gmail.com>
2024-02-01 08:08:38 +0530

    Template

---
+++ index.js
@@ -0,0 +1,26 @@
+let state = {};
+
+const canvas = document.getElementById("game");
+canvas.width = window.innerWidth;
+canvas.width = window.innerHeight;
+const ctx = canvas.getContext("2d");
+
+newGame()
+
+function newGame(){
+    state ={
+
+    }
+    
+    draw();
+
+    function draw(){
+
+    }
+    
+    function throwBomb(){}
+
+    function animate(timestamp){}
+
+    
+}
